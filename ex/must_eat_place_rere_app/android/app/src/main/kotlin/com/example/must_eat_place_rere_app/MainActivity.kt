176420bc00c7d26c14ab49ba9190861f639fe55c
hanjaeng <hanjaeng@gmail.com>
2024-09-06 10:18:35 +0900

    ex

--- ex/must_eat_place_rere_app/android/app/src/main/kotlin/com/example/must_eat_place_rere_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.must_eat_place_rere_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- ex/pixabay_open_ai_app/android/app/src/main/kotlin/com/example/pixabay_open_ai_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.pixabay_open_ai_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
